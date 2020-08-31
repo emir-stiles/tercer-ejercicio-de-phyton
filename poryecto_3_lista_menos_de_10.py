49ec2ff724b9867267a02d0657ec914137e466c5
Emir Stiles <70492089+emir-stiles@users.noreply.github.com>
2020-08-30 22:33:49 -0500

    parte 4 del ejercicio 3 de principiantes de phyton
    pedir un numero al usuario y dependiendo del numero que ingrese se va a imprimir en pantalla los numeros por debajo de este contenida en la lista creada

--- poryecto_3_lista_menos_de_10.py
+++ poryecto_3_lista_menos_de_10.py
@@ -1,2 +1,3 @@
+dato = int(input("ingrese un numero del 1 al 100 "))
 a=[1,1,2,3,5,8,13,21,34,55,89]
-print([cada_uno for cada_uno in a if cada_uno < 5])
+print([cada_uno for cada_uno in a if cada_uno < dato])
